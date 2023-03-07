870bab783162e48fd349459019068fe20a708c8c
Rushikesh Chougule <46069967+rushikesh3299@users.noreply.github.com>
2023-03-07 13:01:26 +0000

    Allow cross origin requests

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const initializeConnectionToDb = require('./db/db.connect.js');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 initializeConnectionToDb();
 
 app.get('/', (req, res) => {
