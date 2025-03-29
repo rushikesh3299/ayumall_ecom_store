2a0c14940a9da52a59e852b7dbcd02be21f789d7
Rushikesh Chougule <rushikeshchougule3299@gmail.com>
2025-03-30 08:18:00 +0900

    Migrate from rplit to local

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const initializeConnectionToDb = async () => {
-  mongoose.connect(process.env.DB_URI,
-    {
+  await mongoose
+    .connect(process.env.DB_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-    },
-  )
-    .then(() => console.log('Successfully connected to db'))
-    .catch((error) => console.error('mongoose connection failed', error))
-}
+    })
+    .then(() => console.log("Successfully connected to db"))
+    .catch((error) => console.error("mongoose connection failed", error));
+};
 
 module.exports = initializeConnectionToDb;
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const initializeConnectionToDb = require('./db/db.connect.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const initializeConnectionToDb = require("./db/db.connect.js");
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,16 +10,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 initializeConnectionToDb();
 
-app.get('/', (req, res) => {
-  res.send("server started")
-})
+app.get("/", (req, res) => {
+  res.send("server started");
+});
 
-app.use('/products', require('./router/public/products.route.js'))
-app.use('/auth', require('./router/public/users.route.js'))
-app.use('/cart', require('./router/protected/cart.route.js'))
-app.use('/wishlist', require('./router/protected/wishlist.route.js'))
-
+app.use("/products", require("./router/public/products.route.js"));
+app.use("/auth", require("./router/public/users.route.js"));
+app.use("/cart", require("./router/protected/cart.route.js"));
+app.use("/wishlist", require("./router/protected/wishlist.route.js"));
 
 app.listen(3000, () => {
-  console.log('server started');
+  console.log("server started");
 });
