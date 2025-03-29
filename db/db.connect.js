5e697f566d200d5185c6216bb9e66fc8ff59d238
Rushikesh Chougule <rushikeshchougule3299@gmail.com>
2025-03-30 08:31:11 +0900

    Update all dependencies

--- db/db.connect.js
+++ db/db.connect.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const initializeConnectionToDb = async () => {
   await mongoose
-    .connect(process.env.DB_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    })
+    .connect(process.env.DB_URI)
     .then(() => console.log("Successfully connected to db"))
     .catch((error) => console.error("mongoose connection failed", error));
 };
