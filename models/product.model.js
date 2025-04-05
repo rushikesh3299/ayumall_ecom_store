5c301f95e62ea560ec7547364c9c550f1f2b364c
Rushikesh Chougule <rushikeshchougule3299@gmail.com>
2025-04-06 07:55:15 +0900

    chng product model

--- models/product.model.js
+++ models/product.model.js
@@ -1,51 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const ProductSchema = new Schema({
+  _id: {
+    type: String,
+    required: "Product ID is required",
+  },
   title: {
     type: String,
-    required: 'Product title is required'
+    required: "Product title is required",
   },
   image: {
-    type: String
+    type: String,
   },
   brand: {
     type: String,
-    required: 'Product brand is required'
+    required: "Product brand is required",
   },
   price: {
     type: Number,
-    required: 'Product price is required'
+    required: "Product price is required",
   },
   weight: {
     type: String,
-    required: 'Product weight is required'
+    required: "Product weight is required",
   },
   description: {
     type: String,
-    required: 'Product description is required'
+    required: "Product description is required",
   },
   ratings: {
     type: Number,
-    required: 'Product rating is required'
+    required: "Product rating is required",
   },
   categoryName: {
     type: String,
-    required: 'Product category is required'
+    required: "Product category is required",
   },
   fastDelivery: {
     type: Boolean,
-    default: false
+    default: false,
   },
   offer: {
     type: Number,
-    default: 0
+    default: 0,
   },
   avalQty: {
     type: Number,
-    required: 'Available quantity information is required'
+    required: "Available quantity information is required",
   },
-})
+});
 
 const Product = mongoose.model("Product", ProductSchema);
 module.exports = { Product };
