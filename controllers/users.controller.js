841e695ffcc0a26dee94130582c3412c7b42dcaa
Rushikesh Chougule <rushikeshchougule3299@gmail.com>
2025-04-15 07:12:20 +0900

    ft:bcrypt for passwd

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const { User } = require("../models/user.model.js");
 
 //@desc Signup user
@@ -6,21 +7,30 @@ const { User } = require("../models/user.model.js");
 //@access Public
 const addNewUser = async (req, res) => {
   try {
-    const userData = req.body;
-    const isUserExists = await User.findOne({ email: userData.email })
+    let userData = req.body;
+    const isUserExists = await User.findOne({ email: userData.email });
     if (isUserExists) {
-      res.status(409).json({ 'sucess': false, 'message': 'User with same email id already exists' })
-    }
-    else {
+      res.status(409).json({
+        sucess: false,
+        message: "User with same email id already exists",
+      });
+    } else {
+      userData.password = await bcrypt.hash(userData.password, 8);
       const newUser = new User(userData);
       const newlyAddedUser = await newUser.save();
-      const jwtToken = jwt.sign({ username: userData.email }, process.env.secreatKey, { expiresIn: '24h' })
-      res.status(201).json({ 'sucess': true, 'data': newlyAddedUser, 'encodedToken': jwtToken })
+      const jwtToken = jwt.sign(
+        { username: userData.email },
+        process.env.secreatKey,
+        { expiresIn: "24h" }
+      );
+      res
+        .status(201)
+        .json({ sucess: true, data: newlyAddedUser, encodedToken: jwtToken });
     }
   } catch (error) {
-    res.status(500).json({ 'sucess': false })
+    res.status(500).json({ sucess: false });
   }
-}
+};
 
 //@desc Login user
 //@route POST -> /auth/login
@@ -28,30 +38,41 @@ const addNewUser = async (req, res) => {
 const logInUser = async (req, res) => {
   try {
     const userData = req.body;
-    const isUserExists = await User.findOne({ email: userData.email })
+    const isUserExists = await User.findOne({ email: userData.email });
     if (isUserExists) {
-      if (isUserExists.password === userData.password) {
+      const isPasswordValid = await bcrypt.compare(
+        userData.password,
+        isUserExists.password
+      );
+      if (isPasswordValid) {
         const foundUser = {
-          'email': isUserExists.email,
-          'firstname': isUserExists.firstname,
-          'lastname': isUserExists.lastname
-        }
-        const jwtToken = jwt.sign({ username: userData.email }, process.env.secreatKey, { expiresIn: '24h' })
-        res.status(200).json({ 'sucess': true, 'encodedToken': jwtToken, 'data': foundUser })
+          email: isUserExists.email,
+          firstname: isUserExists.firstname,
+          lastname: isUserExists.lastname,
+        };
+        const jwtToken = jwt.sign(
+          { username: userData.email },
+          process.env.secreatKey,
+          { expiresIn: "24h" }
+        );
+        res
+          .status(200)
+          .json({ sucess: true, encodedToken: jwtToken, data: foundUser });
+      } else {
+        res.status(200).json({ sucess: false, message: "Wrong password" });
       }
-      else {
-        res.status(200).json({ 'sucess': false, 'message': 'Wrong password' })
-      }
-    }
-    else {
-      res.status(200).json({ 'sucess': false, 'message': 'User with entered email id dosen\'t exists' })
+    } else {
+      res.status(200).json({
+        sucess: false,
+        message: "User with entered email id dosen't exists",
+      });
     }
   } catch (error) {
-    res.status(500).json({ 'sucess': false })
+    res.status(500).json({ sucess: false });
   }
-}
+};
 
 module.exports = {
   addNewUser,
-  logInUser
-}
+  logInUser,
+};
